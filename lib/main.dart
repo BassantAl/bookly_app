5268c13f8dc960bdfdafe7f63829e30a4f5462a1
bassantAli <basant.ali123zx@gmail.com>
2026-09-08 19:47:21 +0300

    bookly ui done

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,25 @@
+import 'package:clean_arch/constants.dart';
+import 'package:clean_arch/core/utils/app_router.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
 
 void main() {
-  runApp(const MyApp());
+  runApp(const Bookly());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Bookly extends StatelessWidget {
+  const Bookly({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-    home: Scaffold()
+    return MaterialApp.router(
+      routerConfig: AppRouter.router,
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: kPrimaryColor,
+        textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+      ),
     );
   }
 }
