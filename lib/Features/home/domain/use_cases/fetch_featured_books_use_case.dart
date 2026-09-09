c2d83ea9f36ce4aaab192c3cd1b8d0f444cee8f2
bassantAli <basant.ali123zx@gmail.com>
2026-09-09 15:37:12 +0300

    refactor to use case class

--- lib/Features/home/domain/use_cases/fetch_featured_books_use_case.dart
+++ lib/Features/home/domain/use_cases/fetch_featured_books_use_case.dart
@@ -1,14 +1,33 @@
 import 'package:clean_arch/Features/home/domain/entities/book_entity.dart';
 import 'package:clean_arch/Features/home/domain/repos/home_repo.dart';
 import 'package:clean_arch/core/errors/failure.dart';
+import 'package:clean_arch/core/use_cases/use_case.dart';
 import 'package:dartz/dartz.dart';
 
-class FetchFeaturedBooksUseCase {
+// class FetchFeaturedBooksUseCase  {
+//   final HomeRepo homeRepo;
+//   FetchFeaturedBooksUseCase({required this.homeRepo});
+
+//   // call method  execute use case
+//   Future<Either<Failure, List<BookEntity>>> call() {
+//     // check permssion
+//     return homeRepo.fetchFeaturedBooks();
+//   }
+// }
+
+class FetchFeaturedBooksUseCase extends UseCase<List<BookEntity>, NoParam> {
   final HomeRepo homeRepo;
   FetchFeaturedBooksUseCase({required this.homeRepo});
 
-  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks() {
-    // check permssion
-    return homeRepo.fetchFeaturedBooks();
+  @override
+  Future<Either<Failure, List<BookEntity>>> call([NoParam? param]) async{
+    return await homeRepo.fetchFeaturedBooks();
   }
 }
+
+// // give me any data type // type present data type
+// abstract class UseCase<type, Param> {
+//   Future<Either<Failure, type>> call([Param param]);
+// }
+
+// class NoParam {}
